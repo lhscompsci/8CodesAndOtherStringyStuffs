97b3995c8f4fb822106c108cac03c3af22d0898c
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-24 16:05:11 -0500

    Assignment Added

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -48,3 +48,27 @@ print(name[-4:8])
 
 for i in range(0, len(name)+1):
     print(name[0:i])
+
+# Searching
+
+print("biv" in name)
+
+if "y" not in name:
+    print("y is not in name")
+else:
+    print("y is in name")
+
+    # String Methods to investigate:
+    # Method        Use Example         Explanation
+    # center        aStr.center(w)
+    # ljust         aStr.ljust(w)
+    # rjust         aStr.rjust(w)
+    # upper         aStr.upper()
+    # lower         aStr.lower()
+    # index         aStr.index(item)
+    # rindex        aStr.rindex(item)
+    # find          aStr.find(item)
+    # rfind         aStr.rfind(item)
+    # replace       aStr.replace(old, new)
+
+    # Be sure to include multiple examples of all of them in use
