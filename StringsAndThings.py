263204ec4e7b5ee740ff2f8ac0fa7a32399056fc
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-24 15:27:12 -0500

    Repetition

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -7,3 +7,21 @@ firstName = "Wilma"
 lastName = "Flintstone"
 
 print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+#   repetition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
