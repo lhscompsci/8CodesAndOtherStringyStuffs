1fd396c3ab32abccbc8e56dc791098993ab48a05
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-28 15:45:39 -0500

    Added mapper.py utility helper functions

---
+++ mapper.py
@@ -0,0 +1,19 @@
+def letterToIndex(letter):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    idx = alphabet.find(letter)
+    if idx == -1:       #means that it wasn't in the alphabet
+        print("error:", letter, "is not in the alphabet")
+    return idx
+
+def indexToLetter(idx):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    letter = ''
+    if idx >= len(alphabet):
+        print("error:", idx, "is too large.")
+    elif idx < 0:
+        print("error:", idx, "is too small.")
+    else:
+        letter = alphabet[idx]
+    return letter
