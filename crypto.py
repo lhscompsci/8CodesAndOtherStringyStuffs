5a6637e13a343ee41aca0f9896e320b26ac3d02b
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-29 15:47:05 -0500

    Added decryption function

--- crypto.py
+++ crypto.py
@@ -13,3 +13,19 @@ def scramble2Encrypt(plainText):
         charCount = charCount + 1
     cipherText = oddChars + evenChars
     return cipherText
+
+
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:]  # halflength to the end
+    oddChars = cipherText[:halfLength]   # 0 to halflength - 1
+    plainText = ""
+
+    for i in range(halfLength):
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+    if len(oddChars) < len(evenChars):
+        plainText = plainText + evenChars[-1]
+
+    return plainText
